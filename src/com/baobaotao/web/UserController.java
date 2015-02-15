1edee5ad309322a703ee01d4cf9d4c0319327acd
zhangxuej <957168943@qq.com>
2015-02-15 09:51:29 +0800

    Change-Id: I7439a0b2058abd7a7bb1ef01c73c4196e76024e9

--- src/com/baobaotao/web/UserController.java
+++ src/com/baobaotao/web/UserController.java
@@ -1,14 +1,28 @@
 package com.baobaotao.web;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.WebUtils;
 
 import com.baobaotao.UserService;
 import com.baobaotao.domain.User;
@@ -99,16 +113,121 @@ public class UserController {
 		mv.addObject("user", new User());
 		return mv;
 	}
-	
-	/**
-	 * ③.请求参数按名称匹配的方式绑定到user属性中、方法返回对应的
-	 * 
-	 * @param user
-	 * @return
-	 */
+
+	// ③请求参数按名称匹配的方式绑定到user的属性中、方法返回对应的字符串代表逻辑视图名
 	@RequestMapping(value = "/handle3")
 	public String handle3(User user) {
 		return "success";
+	}
+
+	// ④直接将HTTP请求对象传递给处理方法、方法返回对应的字符串代表逻辑视图名
+	@RequestMapping(value = "/handle4")
+	public String handle4(HttpServletRequest request) {
+		return "success";
+	}
+
+	@RequestMapping(value = "/handle11")
+	public String handle11(
+			@RequestParam(value = "userName", required = false) String userName,
+			@RequestParam("age") int age) {
+		return "success";
+	}
+
+	@RequestMapping(value = "/handle12")
+	public String handle12(
+			@CookieValue(value = "sessionId", required = false) String sessionId,
+			@RequestParam("age") int age) {
+		return "success";
+	}
+
+	@RequestMapping(value = "/handle13")
+	public String handle13(@RequestHeader("Accept-Encoding") String encoding,
+			@RequestHeader("Keep-Alive") long keepAlive) {
+		return "success";
+	}
+
+	@RequestMapping(value = "/handle14")
+	public String handle14(User user) {
+		return "success";
+	}
+
+	@RequestMapping(value = "/handle21")
+	public void handle21(HttpServletRequest request,
+			HttpServletResponse response) {
+		String userName = request.getParameter("userName");
+		response.addCookie(new Cookie("userName", userName));
+	}
+
+	@RequestMapping(value = "/handle22")
+	public ModelAndView handle22(HttpServletRequest request) {
+		String userName = WebUtils.findParameterValue(request, "userName");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("success");
+		mav.addObject("userName", userName);
+		return mav;
+	}
+
+	@RequestMapping(value = "/handle23")
+	public String handle23(HttpSession session) {
+		session.setAttribute("sessionId", 1234);
+		return "success";
+	}
+
+	@RequestMapping(value = "/success")
+	public String success() {
+		return "success";
+	}
+
+	@RequestMapping(value = "/fail")
+	public String fail() {
+		return "fail";
+	}
+
+	@RequestMapping(value = "/handle24")
+	public String handle24(HttpServletRequest request,
+			@RequestParam("userName") String userName) {
+
+		return "success";
+	}
+
+	@RequestMapping(value = "/handle25")
+	public String handle25(WebRequest request) {
+		String userName = request.getParameter("userName");
+		return "success";
+	}
+
+	@RequestMapping(value = "/handle31")
+	public void handle31(OutputStream os) throws IOException {
+		Resource res = new ClassPathResource("/image.jpg");
+		FileCopyUtils.copy(res.getInputStream(), os);
+	}
+	
+	/**
+	 * 将请求报文体转化为字符串绑定到requestBody入参中
+	 * 
+	 * @param body
+	 * @return
+	 */
+	@RequestMapping(value = "/handle41")
+	public String handle41(@RequestBody String body) {
+		System.out.println(body);
+		return "success";
+	}
+	
+	/**
+	 * 读取一张图片，并将图片数据输出到响应流中，客户端将显示这张图片
+	 * 
+	 * @param imageId
+	 * @return
+	 * @throws IOException
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/handle42/{imageId}")
+	public byte[] handle42(String imageId) throws IOException {
+		System.out.println("load image of " + imageId);
+		Resource res = new ClassPathResource("/image.jpg");
+		byte[] fileData = FileCopyUtils.copyToByteArray(res.getInputStream());
+		return fileData;
 	}
 
 }
---
+++ src/com/baobaotao/web/UserControllerTest.java
@@ -0,0 +1,5 @@
+package com.baobaotao.web;
+
+public class UserControllerTest {
+
+}
